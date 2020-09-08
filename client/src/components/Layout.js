406373ee2a71ac38c74eaa9428d9f59282f98250
Mateusz Wszola <wszola.m@gmail.com>
2020-09-08 12:18:30 +0200

    add bg color

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -3,7 +3,7 @@ import Header from 'components/layout/Header';
 import Footer from 'components/layout/Footer';
 
 const Layout = ({ children }) => (
-  <div className="flex flex-col w-full min-h-screen text-base lg:text-lg antialiased text-gray-900">
+  <div className="flex flex-col w-full min-h-screen text-base lg:text-lg antialiased text-gray-900 bg-white">
     <Header />
     <main className="flex-1 flex flex-col justify-center items-center w-full max-w-screen-xl mx-auto px-2">
       {children}
